8f953bf4a215f4ad97e18f74c1114502fbb5011b
Peter Polidoro <peterpolidoro@gmail.com>
2018-12-11 17:06:20 -0500

    Implemented some methods.

--- src/PCA9685.cpp
+++ src/PCA9685.cpp
@@ -6,4 +6,153 @@
 // ----------------------------------------------------------------------------
 #include "PCA9685.h"
 
+
+void PCA9685::setup(TwoWire & wire,
+  uint8_t slave_address)
+{
+  wire_ptr_ = &wire;
+  slave_address_ = slave_address;
+
+  wire_ptr_->begin();
+  resetAllBusDevices();
+  wake();
+}
+
+void PCA9685::setup(uint8_t address)
+{
+  setup(Wire,address);
+}
+
+void PCA9685::sleep()
+{
+  Mode1Register mode1_register = readMode1Register();
+  mode1_register.fields.sleep = 1;
+  writeByte(MODE1_REGISTER_ADDRESS,mode1_register.data);
+}
+
+void PCA9685::wake()
+{
+  Mode1Register mode1_register = readMode1Register();
+  mode1_register.fields.sleep = 0;
+  mode1_register.fields.ai = 1;
+  writeByte(MODE1_REGISTER_ADDRESS,mode1_register.data);
+  delay(1);
+  if (mode1_register.fields.restart)
+  {
+    mode1_register.fields.restart = 1;
+    writeByte(MODE1_REGISTER_ADDRESS,mode1_register.data);
+  }
+}
+
+uint16_t PCA9685::getMinPwmFrequency()
+{
+  return MICROSECONDS_PER_SECOND / PWM_PERIOD_MAX_US;
+}
+
+uint16_t PCA9685::getMaxPwmFrequency()
+{
+  return MICROSECONDS_PER_SECOND / PWM_PERIOD_MIN_US;
+}
+
+void PCA9685::setPwmFrequency(uint16_t frequency)
+{
+  uint16_t period_us = MICROSECONDS_PER_SECOND / frequency;
+  period_us = constrain(period_us,PWM_PERIOD_MIN_US,PWM_PERIOD_MAX_US);
+  uint8_t prescale = map(period_us,PWM_PERIOD_MIN_US,PWM_PERIOD_MAX_US,PRE_SCALE_MIN,PRE_SCALE_MAX);
+  setPrescale(prescale);
+}
+
+void PCA9685::setChannelOnAndOffTimes(uint8_t channel,
+  uint16_t on_time,
+  uint16_t off_time)
+{
+  wire_ptr_->beginTransmission(slave_address_);
+  wire_ptr_->write(LED0_ON_L_REGISTER_ADDRESS + LED_REGISTERS_SIZE * channel);
+  wire_ptr_->write(on_time);
+  wire_ptr_->write(on_time >> BITS_PER_BYTE);
+  wire_ptr_->write(off_time);
+  wire_ptr_->write(off_time >> BITS_PER_BYTE);
+  wire_ptr_->endTransmission();
+}
+
+void PCA9685::setChannelOnTime(uint8_t channel,
+  uint16_t on_time)
+{
+  wire_ptr_->beginTransmission(slave_address_);
+  wire_ptr_->write(LED0_ON_L_REGISTER_ADDRESS + LED_REGISTERS_SIZE * channel);
+  wire_ptr_->write(on_time);
+  wire_ptr_->write(on_time >> BITS_PER_BYTE);
+  wire_ptr_->endTransmission();
+}
+
+void PCA9685::setChannelOffTime(uint8_t channel,
+  uint16_t off_time)
+{
+  wire_ptr_->beginTransmission(slave_address_);
+  wire_ptr_->write(LED0_ON_L_REGISTER_ADDRESS + LED_REGISTERS_SIZE * channel + LED_REGISTERS_SIZE / 2);
+  wire_ptr_->write(off_time);
+  wire_ptr_->write(off_time >> BITS_PER_BYTE);
+  wire_ptr_->endTransmission();
+}
+
 // private
+
+void PCA9685::writeByte(uint8_t register_address,
+  uint8_t data)
+{
+  wire_ptr_->beginTransmission(slave_address_);
+  wire_ptr_->write(register_address);
+  wire_ptr_->write(data);
+  wire_ptr_->endTransmission();
+}
+
+uint8_t PCA9685::readByte(uint8_t register_address)
+{
+  wire_ptr_->beginTransmission(slave_address_);
+  wire_ptr_->write(register_address);
+  wire_ptr_->endTransmission();
+
+  wire_ptr_->requestFrom(slave_address_,READ_BYTE_COUNT);
+  return wire_ptr_->read();
+}
+
+PCA9685::Mode1Register PCA9685::readMode1Register()
+{
+  Mode1Register mode1_register;
+  mode1_register.data = readByte(MODE1_REGISTER_ADDRESS);
+  // Serial << "restart: " << mode1_register.fields.restart << "\n";
+  // Serial << "extclk: " << mode1_register.fields.extclk << "\n";
+  // Serial << "ai: " << mode1_register.fields.ai << "\n";
+  // Serial << "sleep: " << mode1_register.fields.sleep << "\n";
+  // Serial << "sub1: " << mode1_register.fields.sub1 << "\n";
+  // Serial << "sub2: " << mode1_register.fields.sub2 << "\n";
+  // Serial << "sub3: " << mode1_register.fields.sub3 << "\n";
+  // Serial << "allcall: " << mode1_register.fields.allcall << "\n";
+  return mode1_register;
+}
+
+PCA9685::Mode2Register PCA9685::readMode2Register()
+{
+  Mode2Register mode2_register;
+  mode2_register.data = readByte(MODE2_REGISTER_ADDRESS);
+  // Serial << "outne: " << mode2_register.fields.outne << "\n";
+  // Serial << "outdrv: " << mode2_register.fields.outdrv << "\n";
+  // Serial << "och: " << mode2_register.fields.och << "\n";
+  // Serial << "invrt: " << mode2_register.fields.invrt << "\n";
+  return mode2_register;
+}
+
+void PCA9685::resetAllBusDevices()
+{
+  wire_ptr_->beginTransmission(GENERAL_CALL_SLAVE_ADDRESS);
+  wire_ptr_->write(SWRST);
+  wire_ptr_->endTransmission();
+  delay(10);
+}
+
+void PCA9685::setPrescale(uint8_t prescale)
+{
+  sleep();
+  writeByte(PRE_SCALE_REGISTER_ADDRESS,prescale);
+  wake();
+}
--- src/PCA9685.h
+++ src/PCA9685.h
@@ -7,13 +7,108 @@
 #ifndef PCA9685_H
 #define PCA9685_H
 #include <Arduino.h>
-#include <SPI.h>
+#include <Wire.h>
+
+#include <Streaming.h>
 
 
 class PCA9685
 {
 public:
+  void setup(TwoWire & wire,
+    uint8_t slave_address);
+  void setup(uint8_t slave_address=0x40);
+
+  void sleep();
+  void wake();
+
+  uint16_t getMinPwmFrequency();
+  uint16_t getMaxPwmFrequency();
+  void setPwmFrequency(uint16_t frequency);
+
+  uint16_t getMinTime();
+  uint16_t getMaxTime();
+  void setChannelOnAndOffTimes(uint8_t channel,
+    uint16_t on_time,
+    uint16_t off_time);
+  void setChannelOnTime(uint8_t channel,
+    uint16_t on_time);
+  void setChannelOffTime(uint8_t channel,
+    uint16_t off_time);
+
 private:
+  TwoWire * wire_ptr_;
+  uint8_t slave_address_;
+
+  const static uint8_t GENERAL_CALL_SLAVE_ADDRESS = 0x00;
+  const static uint8_t SWRST = 0b110;
+
+  const static uint8_t READ_BYTE_COUNT = 1;
+
+  void writeByte(uint8_t register_address,
+    uint8_t data);
+  uint8_t readByte(uint8_t register_address);
+
+  const static uint8_t MODE1_REGISTER_ADDRESS = 0x00;
+  union Mode1Register
+  {
+    struct
+    {
+      uint8_t allcall : 1;
+      uint8_t sub3 : 1;
+      uint8_t sub2 : 1;
+      uint8_t sub1 : 1;
+      uint8_t sleep : 1;
+      uint8_t ai : 1;
+      uint8_t extclk : 1;
+      uint8_t restart : 1;
+    } fields;
+    uint8_t data;
+  };
+  Mode1Register readMode1Register();
+
+  const static uint8_t MODE2_REGISTER_ADDRESS = 0x01;
+  union Mode2Register
+  {
+    struct
+    {
+      uint8_t space : 3;
+      uint8_t invrt : 1;
+      uint8_t och : 1;
+      uint8_t outdrv : 1;
+      uint8_t outne : 2;
+    } fields;
+    uint8_t data;
+  };
+  Mode2Register readMode2Register();
+
+  void resetAllBusDevices();
+  void setPrescale(uint8_t prescale);
+
+  const static uint8_t SUBADR1_REGISTER_ADDRESS = 0x02;
+  const static uint8_t SUBADR2_REGISTER_ADDRESS = 0x03;
+  const static uint8_t SUBADR3_REGISTER_ADDRESS = 0x04;
+  const static uint8_t ALLCALLADR_REGISTER_ADDRESS = 0x05;
+
+  const static uint8_t LED0_ON_L_REGISTER_ADDRESS = 0x06;
+  const static uint8_t LED_REGISTERS_SIZE = 4;
+  const static uint8_t BITS_PER_BYTE = 8;
+
+  const static uint8_t ALL_LED_ON_L_REGISTER_ADDRESS = 0xFA;
+  const static uint8_t ALL_LED_ON_H_REGISTER_ADDRESS = 0xFB;
+  const static uint8_t ALL_LED_OFF_L_REGISTER_ADDRESS = 0xFC;
+  const static uint8_t ALL_LED_OFF_H_REGISTER_ADDRESS = 0xFD;
+
+  const static uint8_t PRE_SCALE_REGISTER_ADDRESS = 0xFE;
+  const static uint8_t PRE_SCALE_MIN = 0x03;
+  const static uint8_t PRE_SCALE_MAX = 0xFF;
+  // Measured, should be 1E6/1526=655, variation in chips
+  const static uint16_t PWM_PERIOD_MIN_US = 617;
+  // Measured, should be 1E6/24=41666, variation in chips
+  const static uint16_t PWM_PERIOD_MAX_US = 39525;
+  const static uint32_t MICROSECONDS_PER_SECOND = 1000000;
+
+  const static uint8_t TEST_MODE_REGISTER_ADDRESS = 0xFF;
 };
 
 #endif
